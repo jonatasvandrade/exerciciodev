058d26a376b8e0b36e20ac1b307d86de6169c4c4
jonatasvandrade <50250262+jonatasvandrade@users.noreply.github.com>
2023-04-13 17:42:06 -0300

    exerciciodev

---
+++ src/functions/funcoes.js
@@ -0,0 +1,149 @@
+function validarCPF(cpf) {	
+	cpf = cpf.replace(/[^\d]+/g,'');	
+	if(cpf == '') return false;	
+	// Elimina CPFs invalidos conhecidos	
+	if (cpf.length != 11 || 
+		cpf == "00000000000" || 
+		cpf == "11111111111" || 
+		cpf == "22222222222" || 
+		cpf == "33333333333" || 
+		cpf == "44444444444" || 
+		cpf == "55555555555" || 
+		cpf == "66666666666" || 
+		cpf == "77777777777" || 
+		cpf == "88888888888" || 
+		cpf == "99999999999")
+			return false;		
+	// Valida 1o digito	
+	add = 0;	
+	for (i=0; i < 9; i ++)		
+		add += parseInt(cpf.charAt(i)) * (10 - i);	
+		rev = 11 - (add % 11);	
+		if (rev == 10 || rev == 11)		
+			rev = 0;	
+		if (rev != parseInt(cpf.charAt(9)))		
+			return false;		
+	// Valida 2o digito	
+	add = 0;	
+	for (i = 0; i < 10; i ++)		
+		add += parseInt(cpf.charAt(i)) * (11 - i);	
+	rev = 11 - (add % 11);	
+	if (rev == 10 || rev == 11)	
+		rev = 0;	
+	if (rev != parseInt(cpf.charAt(10)))
+		return false;		
+	return true;   
+}
+
+function validarCNPJ(cnpj) {
+ 
+    cnpj = cnpj.replace(/[^\d]+/g,'');
+ 
+    if(cnpj == '') return false;
+     
+    if (cnpj.length != 14)
+        return false;
+ 
+    // Elimina CNPJs invalidos conhecidos
+    if (cnpj == "00000000000000" || 
+        cnpj == "11111111111111" || 
+        cnpj == "22222222222222" || 
+        cnpj == "33333333333333" || 
+        cnpj == "44444444444444" || 
+        cnpj == "55555555555555" || 
+        cnpj == "66666666666666" || 
+        cnpj == "77777777777777" || 
+        cnpj == "88888888888888" || 
+        cnpj == "99999999999999")
+        return false;
+         
+    // Valida DVs
+    tamanho = cnpj.length - 2
+    numeros = cnpj.substring(0,tamanho);
+    digitos = cnpj.substring(tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(0))
+        return false;
+         
+    tamanho = tamanho + 1;
+    numeros = cnpj.substring(0,tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(1))
+          return false;
+           
+    return true;
+    
+}
+
+function SelecionaDoc(tipodoc){
+    if(tipodoc){	
+        switch(tipodoc){
+            case 'cpf':
+                document.getElementById("box-cnpj").style.display ="none"
+                document.getElementById("box-cpf").style.display ="block"
+                alert("teste");
+                break;
+            case 'cnpj':
+            document.getElementById("box-cpf").style.display ="none"
+            document.getElementById("box-cnpj").style.display ="block"
+            alert("teste");
+            break;
+            
+        }
+    }else{
+        alert("Selecione um tipo de documento")
+
+    }
+    
+
+}
+
+function validaNome(){
+    if(document.getElementById("nome").value != "" && length < 2){
+        return true
+        
+    }else{
+        return false	
+    }
+}
+
+function validaForm(){
+    alert("teste Form")
+    var nome = document.getElementById("nome").value;
+    var cpf = document.getElementById("cpf").value;
+    if(nome != "" && length < 2){
+        alert("nome verdadeiro")
+        alert("o nome é "+nome)
+        
+    }else{
+        alert("nome Falso")
+        die;	
+    }
+    //verifica cpf
+    alert("o CPF é"+cpf);
+    var verificaCPF = validarCPF(cpf)
+
+    if(cpf !== ""){
+        alert("valida cpf")
+        if(verificaCPF){
+            alert('CPF válido')
+        }else{
+            alert('cpf inválido')
+        }
+    }
+    
+}
