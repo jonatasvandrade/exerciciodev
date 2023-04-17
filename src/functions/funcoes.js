5bd901ba30f7901f4f3a4675cfe6e77c7aec32b0
jonatasvandrade <50250262+jonatasvandrade@users.noreply.github.com>
2023-04-17 12:40:57 -0300

    Valida CEP e Telefone

--- src/functions/funcoes.js
+++ src/functions/funcoes.js
@@ -144,6 +144,19 @@ function validacaoEmail(field) {
     }
 }
 
+const handlePhone = (event) => {
+    let input = event.target
+    input.value = phoneMask(input.value)
+  }
+  
+  const phoneMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"($1) $2")
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+    return value
+  }
+
 function validaForm(event){
     event.preventDefault()
     //alert("teste Form")
